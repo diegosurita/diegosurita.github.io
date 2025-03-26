2e50ae6c0c65550cf3f6f5bcabf69f40096fda10
Diego Surita <suritadev@gmail.com>
2025-03-26 16:03:36 -0300

    chore: add google analytics gtag script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KEGV160RYL"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-KEGV160RYL');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
