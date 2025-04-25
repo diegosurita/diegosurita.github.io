5e4d2adb6a94950f9d607e421f6955343968a01d
Diego Surita <suritadev@gmail.com>
2025-04-25 18:07:48 -0300

    feat: create about page
    issue: #9

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import Link from "next/link";
+import { useParams } from "next/navigation";
 import NavLink from "@/components/common/navlink";
 
 export default function Navbar() {
+  const { lang } = useParams();
+
   return (
     <nav className="flex items-center justify-between h-24 mb-4">
       <Link href="/" className="text-2xl font-bold hover:text-gray-400">
@@ -9,8 +14,10 @@ export default function Navbar() {
         <p className="text-sm text-gray-500">Software Engineer</p>
       </Link>
       <ul className="flex gap-4">
-        <NavLink href="/about">About</NavLink>
-        <NavLink className="hidden" href="/blog">Blog</NavLink>
+        <NavLink href={`/${lang}/about`}>About</NavLink>
+        <NavLink className="hidden" href={`/${lang}/blog`}>
+          Blog
+        </NavLink>
         <NavLink href="mailto:suritadev@gmail.com">Contact</NavLink>
       </ul>
     </nav>
